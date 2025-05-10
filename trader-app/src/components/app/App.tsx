6173b9edfa254d1f50548fb71243811973752397
murzin66 <mikhailmurzinin@gmail.com>
2025-05-10 17:09:59 +0300

    initialize project

---
+++ trader-app/src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import {Route, Routes} from 'react-router-dom';
+import MainPage from "../../pages/main-page/main-page";
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        path = "/"
+        element = {<MainPage/>}
+        />
+
+    </Routes>
+  );
+}
+
+export default App;
---
+++ trader-app/src/pages/main-page/main-page.tsx
@@ -0,0 +1,8 @@
+function MainPage () {
+  return (
+    <div id = "main-page">
+      Главная страница
+    </div>
+  );
+}
+export default MainPage;
